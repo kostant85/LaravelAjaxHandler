9e7549eb2f606e24f9fde984ddbba236883ed4cf
kostant1985 <kostant21@yahoo.com>
2020-03-15 19:58:19 +0200

    added laravel 7.0 supports

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -3,30 +3,30 @@
 namespace Kostant\LaravelAjaxHandler\Exeptions;
 
 
-use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Session\TokenMismatchException;
 use Kostant\LaravelAjaxHandler\RespondsJson;
+use Throwable;
 
 class Handler extends \App\Exceptions\Handler
 {
-    use RespondsJson;
+	use RespondsJson;
 
-    public function render($request, Exception $exception)
-    {
-        if ($exception instanceof TokenMismatchException && $request->expectsJson()) {
+	public function render($request, Throwable $exception)
+	{
+		if ($exception instanceof TokenMismatchException && $request->expectsJson()) {
 
-            return $this->jsonError('Opps! Seems you couldn\'t submit form for a long time. Please refresh the page and try again.', [], 403);
-        }
+			return $this->jsonError('Opps! Seems you couldn\'t submit form for a long time. Please refresh the page and try again.', [], 403);
+		}
 
-        if ($exception instanceof AuthenticationException && $request->expectsJson()) {
-            session()->flash('Session expired.', 'warning');
+		if ($exception instanceof AuthenticationException && $request->expectsJson()) {
+			session()->flash('Session expired.', 'warning');
 
-            return $this->jsonError($exception->getMessage(), [
-                'redirectUrl' => route('login'),
-            ], 401);
-        }
+			return $this->jsonError($exception->getMessage(), [
+				'redirectUrl' => route('login'),
+			], 401);
+		}
 
-        return parent::render($request, $exception);
-    }
+		return parent::render($request, $exception);
+	}
 }
